cbe6103daa8fc183deb22b4a62d1807879d14196
Rylie Castell <rylie.castell@gmail.com>
2023-09-09 01:50:52 +0200

    add pokemon traits

--- components/PokemonOfTheDay.js
+++ components/PokemonOfTheDay.js
@@ -1,13 +1,33 @@
 import styled from "styled-components";
+import { useState } from "react";
 import usePokemon from "../swr/usePokemon";
 
-const Wrapper = styled.div``;
-const Title = styled.h2``;
-const PokemonName = styled.h3``;
-const Image = styled.img``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 16em;
+`;
+
+const Title = styled.h2`
+  font-size: 1.8em;
+`;
+
+const PokemonName = styled.h3`
+  font-size: 1.4em;
+`;
+
+const Image = styled.img`
+  height: 14em;
+`;
+
+const Trait = styled.p`
+  font-size: 1.2em;
+`;
 
 export default function PokemonOfTheDay() {
-  const { pokemon, isLoading, isError } = usePokemon("25");
+  const [id, setId] = useState("25");
+  const { pokemon, isLoading, isError } = usePokemon(id);
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -16,14 +36,26 @@ export default function PokemonOfTheDay() {
   }
 
   const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+  const ability = pokemon.abilities[0].ability.name;
+  const type = pokemon.types[0].type.name;
+  const index = pokemon.game_indices[0].game_index;
+  const version = pokemon.game_indices[0].version.name;
   return (
     <Wrapper>
       <Title>Pokemon of the Day</Title>
       <PokemonName>{name}</PokemonName>
       <Image
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-        alt="Picture of Pikachu"
+        src={
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+          id +
+          ".png"
+        }
+        alt={"Picture of " + name}
       />
+      <Trait>Ability: {ability}</Trait>
+      <Trait>Type: {type}</Trait>
+      <Trait>Index: {index}</Trait>
+      <Trait>Edition: {version}</Trait>
     </Wrapper>
   );
 }
