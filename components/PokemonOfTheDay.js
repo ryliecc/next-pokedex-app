76ec133ac3da1774173817efeae8da874e31e91b
Rylie Castell <rylie.castell@gmail.com>
2023-09-09 11:23:16 +0200

    add random pokemon id

--- components/PokemonOfTheDay.js
+++ components/PokemonOfTheDay.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import usePokemon from "../swr/usePokemon";
 
 const Wrapper = styled.div`
@@ -28,34 +28,38 @@ const Trait = styled.p`
 export default function PokemonOfTheDay() {
   const [id, setId] = useState("25");
   const { pokemon, isLoading, isError } = usePokemon(id);
+
+  const name = pokemon?.name.charAt(0).toUpperCase() + pokemon?.name.slice(1);
+  const ability = pokemon?.abilities[0].ability.name;
+  const type = pokemon?.types[0].type.name;
+  const index = pokemon?.game_indices[0].game_index;
+  const version = pokemon?.game_indices[0].version.name;
+  const spriteSrc = pokemon?.sprites.front_default;
+
+  function getRandomId(max) {
+    const int = Math.floor(Math.random() * max);
+    return int.toString();
+  }
+
+  useEffect(() => {
+    setId(getRandomId(100));
+  }, []);
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
   if (isError) {
     return <div>Error...</div>;
   }
-
-  const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-  const ability = pokemon.abilities[0].ability.name;
-  const type = pokemon.types[0].type.name;
-  const index = pokemon.game_indices[0].game_index;
-  const version = pokemon.game_indices[0].version.name;
   return (
     <Wrapper>
       <Title>Pokemon of the Day</Title>
       <PokemonName>{name}</PokemonName>
-      <Image
-        src={
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
-          id +
-          ".png"
-        }
-        alt={"Picture of " + name}
-      />
+      <Image src={spriteSrc} alt={"Picture of " + name} />
       <Trait>Ability: {ability}</Trait>
       <Trait>Type: {type}</Trait>
       <Trait>Index: {index}</Trait>
-      <Trait>Edition: {version}</Trait>
+      <Trait>Version: {version}</Trait>
     </Wrapper>
   );
 }
